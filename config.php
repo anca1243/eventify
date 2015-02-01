af5e226d767607e8b6becaee1074ecb5c89a95ab
Harry Ward <harry.ward@student.manchester.ac.uk>
2015-02-01 16:05:47 +0000

    Stable version

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 $group_dbnames = array("dbhost.cs.man.ac.uk", "2014_comp10120_x2", "mbax4hw2", "BestTeam2014");
-$facebook_appID = "837275619665461";
-$facebook_appSec = "6aafcab796d6f35bd3eeca0b2ea31586";
-$url = "http://130.88.189.32/"
+$facebook_appID = "837257679667255";
+$facebook_appSec = "89b060314552c984654f8aafc584526b";
+$url = "https://web.cs.manchester.ac.uk/mbax4hw2/eventify"
 ?>
 
 <script>
-  var facebook_appid = "837275619665461";
-  var facebook_appsec = "6aafcab796d6f35bd3eeca0b2ea31586";
+  var facebook_appid = "837257679667255";
+  var facebook_appsec = "89b060314552c984654f8aafc584526b";
 </script>
