dfcf1c22e56ea9769f3dd16ac5c368e5653d626c
Harry Ward <harry.ward@student.manchester.ac.uk>
2015-02-01 13:46:38 +0000

    IT WAS THE SLASH

--- index.php
+++ index.php
@@ -8,5 +8,12 @@
   <h1>Welcome to eventify</h1>
   <h2>This is an alpha version of the site, functionality can and will change</h2>
   <h3>Alpha v0.0.1</h3>
+  <?php
+    echo "<div id='satan'>";
+    for ($i = 1; $i <= 10; $i++) {
+      echo "<marquee class='satan' scrollamount='".($i*10)."'><h1 class='satan'>IT WAS THE SLASH</h1>/SLASH/SLASH//:(/<img src='http://www.godandscience.org/images/devil.jpg'></marquee>";
+    }
+    echo "</div>";
+  ?>
  </body>
 </html>
