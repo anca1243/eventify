5b903dd1c3ffe6653d76c0300067f0123322ecaa
Harry Ward <Harry.ward@student.manchester.ac.uk>
2015-02-18 23:54:02 +0000

    FIXING GIT AGAIN

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <?php require("style/linkcss.php"); ?>
  </head>
  <body>
+  <!-- Easy import of header -->
   <?php require("style/header.php"); ?>
   <h1>Welcome to eventify</h1>
   <h2>This is an alpha version of the site, functionality can and will change</h2>
