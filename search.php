0b80cf7462df874ebb1bfe3afd760647771031b4
Harry Ward <Harry.ward@student.manchester.ac.uk>
2015-02-23 13:55:06 +0000

    Made searching all on one page yay

--- search.php
+++ search.php
@@ -5,37 +5,58 @@
   </head>
 
   <body>
-    <?php require("style/header.php"); ?>
+    <?php require("style/header.php");
+          require("database.php"); 
+    ?>
     <div id="content">
-      <h1>Search Events</h1>
-      <div id="searchForm" class="eventForm">
-        <form id="search" action="searchResults.php" method="get">
+      <div class="row">
+        <h1>Search Events</h1>
+      </div>
+      <div class="row">
+       <div class="col s3">
+        <div id="searchForm" class="eventForm">
+         <form id="search" action="search.php" method="get">
           <div class="input-field">
             <label for="evtitle">Name of Event</label>
-            <input type="text" name="evtitle" />
+            <input type="text" name="evtitle" value='<?php echo $_GET['evtitle'] ?>' />
           </div>
          
-         <div class="input-field">
+          <div class="input-field">
            <label for="evdesc">Additional Search Terms</label>
-           <input type="text" name="evdesc" />
-         </div>
+           <input type="text" name="evdesc" value='<?php echo $_GET['evdesc'] ?>' />
+          </div>
  
-         <div class="input-field">
+          <div class="input-field">
            <label for="evdate">Date</label>
-           <input type="text" class="datepicker" name="evdate" />
-         </div>
+           <input type="text" class="datepicker" name="evdate" value='<?php echo $_GET['evdate'] ?>' />
+          </div>
 
-         <div class="input-field">
+          <div class="input-field">
            <label for="evpostcode">Event Postcode</label>
-           <input type="text" name="evpostcode" /> 
-        </div>
+           <input type="text" name="evpostcode" value='<?php echo $_GET['evpostcode'] ?>'/> 
+          </div>
    
-        <button type="submit" class="btn waves-effect waves-light">
+          <button type="submit" class="btn waves-effect waves-light">
              Search
              <i class="mdi-content-send right"></i>
-        </button>
-      </form>
-      </div>    
+          </button>
+         </form>
+        </div> <!-- End of form div -->
+       </div> <!-- End of search terms columns -->
+      <div class="col s9">
+       <?php
+        //Retrieve vars
+        $name = $_GET['evtitle'];
+        $date  = $_GET['evdate'];
+        $post  = $_GET['evpostcode'];
+        //For the database
+        $description=$_GET['evdesc'];
+        $location="";
+        $results = search_events($name, $location, $date, $description, $post);
+        displayResults($results);
+       ?>
+      </div>  
+     </div> <!-- End of results -->   
     </div>
   </body>
 
