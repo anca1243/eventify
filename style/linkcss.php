1b790af2e0fcda31f5a44c3e9bfc0eb4fbd9a569
Harry Ward <Harry.ward@student.manchester.ac.uk>
2015-02-23 12:48:28 +0000

    Gave footer some CSS

--- style/linkcss.php
+++ style/linkcss.php
@@ -7,4 +7,9 @@
       href="css/houseStyle.css"
       media="screen,projection"
 />
-
+<!--Style for footer, should be moved somewhere else though-->
+<style>
+#footer {
+    text-align:center;
+}
+</style>
