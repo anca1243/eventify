94204c81112026104d5f851246e8c9a4a4423a66
root <root@localhost.localdomain>
2015-02-24 02:45:59 +0000

    Added current events on the main page. Hopefully.

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
   <h1>Welcome to eventify</h1>
   <h2>This is an alpha version of the site, functionality can and will change</h2>
   <h3>Alpha v0.0.1</h3>
+  <?php
+   $results = search_events("", "", date('d M y'), "", "");
+   displayResults($results);
+  ?>
+
  <?php require("style/footer.php"); ?>
  </body>
 </html>
