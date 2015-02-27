57a6615c6742452db2945fe39f9462adf6eb856f
Harry Ward <Harry.ward@student.manchester.ac.uk>
2015-02-27 17:43:19 +0000

    Modified homepage layout

--- index.php
+++ index.php
@@ -6,32 +6,42 @@
  <body>
   <!-- Easy import of header -->
   <?php require("style/header.php");
-        require("database.php"); ?>
-  <h1 class="welcome">Welcome to eventify</h1>
-  <div class="row" id="splash">
-    <div class="col s4">
-      <i id="icon" class="mdi-action-event"></i>
-      <h3>TAGLINE</h3>
-      <p>Something something find events. Something something cure boredom</p>
-    </div>
-    <div class="col s4">
-      <i id="icon" class="mdi-action-account-child"></i>
-      <h3>TAGLINE</h3>
-      <p>Something something user interaction. Something something fun with friends</p>
-    </div>
-    <div class="col s4">
-      <i id="icon" class="mdi-action-done-all"></i>
-      <h3>TAGLINE</h3>
-      <p>Something something yes indeed. Something something else</p>
-    </div>
+        require("database.php");
+        if (!isset($_SESSION["session"])) {
+          echo "
+           <h1 class=\"welcome\">Welcome to eventify</h1>
+           <div class=\"row\" id=\"splash\">
+            <div class=\"col s4\">
+             <i id=\"icon\" class=\"mdi-action-event\"></i>
+             <h3>TAGLINE</h3>
+             <p>Something something find events. 
+                Something something cure boredom</p>
+           </div>
+           <div class=\"col s4\">
+            <i id=\"icon\" class=\"mdi-action-account-child\"></i>
+            <h3>TAGLINE</h3>
+            <p>Something something user interaction. 
+               Something something fun with friends</p>
+           </div>
+           <div class=\"col s4\">
+            <i id=\"icon\" class=\"mdi-action-done-all\"></i>
+            <h3>TAGLINE</h3>
+            <p>Something something yes indeed. 
+               Something something else</p>
+          </div>
 
-  </div>
-  <h4>Events happening today, <?php echo date('d M y'); ?></h4> 
-  <?php
-   $results = search_events("", "", date('d M y'), "", "");
-   displayResults($results);
-  ?>
-
+          </div>";
+  } else {
+    require("geoip.php");
+    $user = fbRequest("/me");
+    echo "Welcome back, ".$user['first_name']. " ".$user['last_name'];
+    $location = getLocation();
+    echo "Your location: ".$location['zipCode'];
+    echo "<h4>Events happening today, ". date('d M y') ."</h4>"; 
+    $results = search_events("", "", date('d M y'), "", "");
+    displayResults($results);
+  }
+ ?>
  <?php require("style/footer.php"); ?>
  </body>
 </html>
