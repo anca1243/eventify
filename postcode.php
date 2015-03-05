eec48227a7f9cba61404c0f8f5183c1b90308f50
Harry Ward <Harry.ward@student.manchester.ac.uk>
2015-03-05 01:06:42 +0000

    Modified colours of inputs and labels

--- postcode.php
+++ postcode.php
@@ -11,17 +11,22 @@
  <!--Form to allow manual setting of postcode 
      uses POST. I find this strangely satisfying
  -->
+ <div class="row">
  <form action="index.php" method="post" name="form" class="eventForm">
+  <div class="col s6">
    <div class="input-field">
      <label for="postcode">Your Postcode</label>
      <input type="text" name="postcode"></input>
    </div>
+ </div>
+ <div class="col s2">
    <button type="button" onclick=subcheckPostcode() class="btn waves-effect waves-light">
      Submit
      <i class="mdi-content-send right"></i>
    </button>
+  </div>
  </form>
-
+ </div>
  <?php require("style/footer.php"); ?>
  </body>
  <script src="js/common.js">
@@ -31,7 +36,7 @@
      if (checkPostcode(document.getElementsByName("postcode")[0].value))
        document.getElementsByName("form")[0].submit();
      else 
-       toast("Invalid postcode!");
+       toast("Invalid postcode!", 4000);
    }
  </script>
 </html>
