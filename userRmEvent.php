f8bcedaaa78d0b3d4d06c8faf08f195c90e1c8bd
Harry Ward <Harry.ward@student.manchester.ac.uk>
2015-03-05 20:38:23 +0000

    Allow user to "not go to" events

--- userRmEvent.php
+++ userRmEvent.php
@@ -5,10 +5,12 @@
    require("style/header.php");
    require("database.php");
    $con = connect();
-   $stmt = $con->prepare("DELETE FROM UserFollows 
-                          WHERE User1=? AND User2=?;");
+   $stmt = $con->prepare("DELETE FROM UserEvents WHERE UserID=?
+                          AND EventID=?;");
    $stmt->bind_param('si', $_SESSION["id"], $_POST["id"]);
+   echo "Hello:" .$_SESSION["id"]. " " . $_POST["id"];
    $stmt->execute();
+   echo "It is dun.";
    header("Location: user.php?id='".$_SESSION["id"]."'"); 
  ?>
 </html>
