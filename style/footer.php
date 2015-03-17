c8bd623c798e6f6e5efd7a518b024d1aaada006c
Harry Ward <Harry.ward@student.manchester.ac.uk>
2015-03-17 10:58:14 +0000

    Added jquery libs locally

--- style/footer.php
+++ style/footer.php
@@ -26,7 +26,7 @@
 </body>
    <!--  Scripts-->
   
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="../jquery-1.11.2.js"></script>
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
   <script>
