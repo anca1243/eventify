9fa1145e8d643b4731cf2233690943d7fd7100bf
Harry Ward <Harry.ward@student.manchester.ac.uk>
2015-03-20 12:31:19 +0000

    COME ON GEOIP

--- geoIP.php
+++ geoIP.php
@@ -6,7 +6,8 @@ function getLocation() {
   $ipLite = new ip2location_lite;
   $ipLite->setKey($API_KEY);
   //Get user's city based on their IP addr 
-  $locations = $ipLite->getCity($_SERVER['REMOTE_ADDR']);
+  $locations = file_get_contents("http://api.ipinfodb.com/v3/ip-city/?key=32de137900caa0fa4c55e85859b44231c1f60112d0b143e0db33248e40553450&ip=".$_SERVER['REMOTE_ADDR'];
+  echo $locations;
   //Detect errors (does not handle)
   $errors = $ipLite->getError();
   print_r($errors);
